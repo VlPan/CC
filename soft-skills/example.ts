4f7b9d522271134a2ec7ff90c355b8515ad66341
UPantsiukhou <UPantsiukhou@slb.com>
2021-01-17 20:59:32 +0300

    ss

---
+++ soft-skills/example.ts
@@ -0,0 +1,12 @@
+/*
+1.1	Information expert
+1.2	Creator
+1.3	Controller
+1.4	Indirection
+1.5	Low coupling
+1.6	High cohesion
+1.7	Polymorphism
+1.8	Protected variations
+1.9	Pure fabrication
+*/
+
